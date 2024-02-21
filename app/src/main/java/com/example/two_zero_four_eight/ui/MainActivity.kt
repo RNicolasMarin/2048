dc8fdc1a3d9ae922bb9eb603914c3785b611dbe0
Nicolas Marin <nicolasmarin1312@gmail.com>
2024-02-21 20:46:05 -0300

    App's icon modified to being adaptive and splash screen added

--- app/src/main/java/com/example/two_zero_four_eight/ui/MainActivity.kt
+++ app/src/main/java/com/example/two_zero_four_eight/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.two_zero_four_eight.ui.theme.TwoZeroFourEightTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             TwoZeroFourEightTheme {
                 Surface(
